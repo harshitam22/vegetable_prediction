7be41dda9728b4bc52cb6ce2b5840e760be4fb28
Harshita Mehboobani <harshitamehboobani22@gmail.com>
2023-08-31 23:13:29 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import numpy as np
+from flask import Flask, render_template, request, redirect
+from flask_cors import CORS, cross_origin
+import pickle
+import pandas as pd
+
+app=Flask(__name__)
+
+cors=CORS(app)
+model=pickle.load(open('model.pkl','rb'))
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+@cross_origin()
+def predict():
+
+    veg = request.form.get('veg')
+    season =request.form.get('season')
+    temp = request.form.get('temp')
+    mon = request.form.get('mon')
+    condition = request.form.get('condition')
+    disaster = request.form.get('disaster')
+    
+    prediction = model.predict([[int(veg),int(season),int(temp),
+                              int(mon),int(condition),int(disaster)]])
+    print(prediction)
+
+    return str(np.round(prediction[0][0],2))
